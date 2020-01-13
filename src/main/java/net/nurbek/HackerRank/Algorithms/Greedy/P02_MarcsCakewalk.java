d21e74d1779d0b4caa763ae0f2692650163cd1bf
Nurbek T <nurbekinho@gmail.com>
2020-01-13 18:51:13 +0100

    Solution for "Greedy" problem 2.

---
+++ src/main/java/net/nurbek/HackerRank/Algorithms/Greedy/P02_MarcsCakewalk.java
@@ -0,0 +1,27 @@
+package net.nurbek.HackerRank.Algorithms.Greedy;
+
+import java.util.Arrays;
+
+public class P02_MarcsCakewalk
+{
+    public static void main(String[] args)
+    {
+        System.out.println(marcsCakewalk(new int[]{1, 3, 2}));
+    }
+
+    static long marcsCakewalk(int[] calorie)
+    {
+        Arrays.sort(calorie);
+
+        long min = 0;
+
+        for(int i = 0; i < calorie.length; i++)
+        {
+            min += Math.pow(2, i) * calorie[calorie.length - i - 1];
+        }
+
+        return min;
+    }
+}
+
+// SOLVED //
