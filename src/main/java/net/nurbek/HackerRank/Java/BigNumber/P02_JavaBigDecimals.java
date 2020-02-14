68ab67ca94a72fac32ba5d1434c961a717807987
Nurbek T <nurbekinho@gmail.com>
2020-02-14 18:08:18 +0100

    Solution for "Maths - Big Number" problem 2.

---
+++ src/main/java/net/nurbek/HackerRank/Java/BigNumber/P02_JavaBigDecimals.java
@@ -0,0 +1,24 @@
+package net.nurbek.HackerRank.Java.BigNumber;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class P02_JavaBigDecimals
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String [] s = new String[n+2];
+        for(int i = 0; i < n; i++)
+        {
+            s[i] = sc.next();
+        }
+      	sc.close();
+
+        for(int i = 0; i < n; i++)
+        {
+            System.out.println(s[i]);
+        }
+	}
+}
