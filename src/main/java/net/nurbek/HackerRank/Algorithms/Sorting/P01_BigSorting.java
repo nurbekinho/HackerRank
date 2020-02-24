1f4a4dafc6be6b51447187e4ec235c6ce083513a
Nurbek T <nurbekinho@gmail.com>
2020-02-24 20:21:56 +0100

    Replace with lambda

--- src/main/java/net/nurbek/HackerRank/Algorithms/Sorting/P01_BigSorting.java
+++ src/main/java/net/nurbek/HackerRank/Algorithms/Sorting/P01_BigSorting.java
@@ -9,27 +9,23 @@ public class P01_BigSorting
     {
         String [] numbers = {"31415926535897932384626433832", "314159265358979323846264338327", "31415926535897932384626433832795", "3141592653589793238462643383279", "1", "3", "10", "3", "5"};
         
-        Arrays.sort(numbers, new Comparator<String>()
-        {
-		    public int compare(String a, String b)
-		    {
-				if (a.length() > b.length())
-					return 1;
-				else if (a.length() < b.length())
-					return -1;
-				else if (a.length() == b.length())
+        Arrays.sort(numbers, (a, b) -> {
+			if (a.length() > b.length())
+				return 1;
+			else if (a.length() < b.length())
+				return -1;
+			else
+			{
+				for (int i = 0; i < a.length(); i++)
 				{
-					for (int i = 0; i < a.length(); i++)
-					{
-						if (a.charAt(i) > b.charAt(i))
-						return 1;
-						if (a.charAt(i) < b.charAt(i))
-						return -1;
-					}
+					if (a.charAt(i) > b.charAt(i))
+					return 1;
+					if (a.charAt(i) < b.charAt(i))
+					return -1;
 				}
-				
-				return 0;
 			}
+
+			return 0;
 		});
 
 		System.out.println(Arrays.toString(numbers));
