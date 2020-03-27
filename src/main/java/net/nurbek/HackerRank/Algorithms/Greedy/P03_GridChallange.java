07c42650bea3573fe5756fac5dc4221a54335936
Nurbek T <nurbekinho@gmail.com>
2020-03-27 18:05:21 +0100

    Refactor code

--- src/main/java/net/nurbek/HackerRank/Algorithms/Greedy/P03_GridChallange.java
+++ src/main/java/net/nurbek/HackerRank/Algorithms/Greedy/P03_GridChallange.java
@@ -4,8 +4,7 @@ import java.util.Arrays;
 
 public class P03_GridChallange
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println(gridChallenge(new String[]{"ebacd",
                                                     "fghij",
                                                     "olmkn",
@@ -19,20 +18,16 @@ public class P03_GridChallange
                                                     "xywuv"}));
     }
 
-    private static String gridChallenge(String[] grid)
-    {
-        for(int i = 0; i < grid.length; i++)
-        {
+    private static String gridChallenge(String[] grid) {
+        for(int i = 0; i < grid.length; i++) {
             char[] chars = grid[i].toCharArray();
             Arrays.sort(chars);
 
             grid[i] = new String(chars);
         }
 
-        for(int i = 0; i < grid.length - 1; i++)
-        {
-            for(int j = 0; j < grid[i].length(); j++)
-            {
+        for(int i = 0; i < grid.length - 1; i++) {
+            for(int j = 0; j < grid[i].length(); j++) {
                 if(grid[i].charAt(j) > grid[i + 1].charAt(j)) return "NO";
             }
         }
